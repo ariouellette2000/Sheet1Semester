9b09c6b440a055f9635030e6b45e8587cc4130d2
ariouellette2000 <43147334+ariouellette2000@users.noreply.github.com>
2019-08-27 11:39:01 -0400

    Start the link for index and testimonial

---
+++ Sheet1Semester/Controllers/Semester5Controller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sheet1Semester.Controllers
+{
+    public class Semester5Controller : Controller
+    {
+        // GET: Semester5
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Semester5()
+        {
+            return View();
+        }
+    }
+}
---
+++ Sheet1Semester/Controllers/TestimonialController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sheet1Semester.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        // GET: Testimonial
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Testamonial()
+        {
+            return View();
+        }
+    }
+}
